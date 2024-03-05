28e815ff4c2f09298fca06b1245bc095e43fb5e8
coronelee <79922854+coronelee@users.noreply.github.com>
2024-03-05 15:08:17 +0300

    add

---
+++ .next/static/DRTworux-fu5HM2R3t7C9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/181-6ac8494bd7f0940f.js","static/css/443bc3adbb530414.css","static/chunks/pages/index-9cf38ef72bfa4941.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
